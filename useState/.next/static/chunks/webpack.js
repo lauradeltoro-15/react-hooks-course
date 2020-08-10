31d127e5f27b97f27ee7df4de3e312b93a6237fa
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-08-10 13:58:40 +0100

    Add conf site example

--- useState/.next/static/chunks/webpack.js
+++ useState/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d3536037e08e26f9d107";
+/******/ 	var hotCurrentHash = "665c93789fcad809b153";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
